e45e0e77ea661cc7330a3132174e18bd4697c6c2
Ilie Todirascu <ilie.todirascu@amdaris.com>
2022-09-17 09:08:39 +0300

    initial commit

---
+++ Kitchen/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Kitchen.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly ILogger _logger;
+
+        public TestController(IHttpClientFactory clientFactory, ILogger<TestController> logger)
+        {
+            _clientFactory = clientFactory;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            _logger.LogInformation("I am here");
+            var client = _clientFactory.CreateClient();
+            HttpResponseMessage response = await client.GetAsync("http://localhost:60500/Weatherforecast");
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+            })
+            .ToArray();
+        }
+    }
+}
