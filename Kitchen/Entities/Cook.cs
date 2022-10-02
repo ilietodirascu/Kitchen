a4679c2c19c1c2cf522a3231773528bd4aeb42ec
Ilie Todirascu <ilie.todirascu@amdaris.com>
2022-10-02 14:33:35 +0300

    Fixed some Bugs

--- Kitchen/Entities/Cook.cs
+++ Kitchen/Entities/Cook.cs
@@ -14,20 +14,35 @@ namespace Kitchen.Entities
         private int _profieciency;
         public int Proficiency { get { return _profieciency; } set { _startingProficiency = _startingProficiency == 0 ? value : _startingProficiency; _profieciency = value; } }
         public string CatchPhrase { get; set; } = "I like cooking multiple dishes at once";
+        private static object _cookLock = new object();
 
         public void PrepareFood(Food food)
         {
-            lock (Utility.Lock)
+            lock (_cookLock)
             {
-                if (Proficiency >= _startingProficiency) Proficiency--;
                 //Task.Delay(food.PreparationTime * 250).ContinueWith(_ =>
                 //{
                 //    Utility.AddFood(food);
-                //    if (Proficiency < _proficiency) Proficiency++;
+                //    if (Proficiency < _startingProficiency) Proficiency++;
                 //});
+                this.Proficiency--;
                 Thread.Sleep(food.PreparationTime * 250);
-                Utility.AddFood(food);
                 if (Proficiency < _startingProficiency) Proficiency++;
+                Utility.AddFood(food);
+            }
+        }
+        public void DoWork()
+        {
+            lock (Utility.Lock)
+            {
+                if (Utility.FoodsToPrepare.IsEmpty) return;
+                var potentialFood = Utility.FoodsToPrepare.First();
+                if (potentialFood.Complexity > Rank || Proficiency < 1) return;
+                Utility.FoodsToPrepare.TryDequeue(out Food food);
+                new Thread(() =>
+                {
+                    this.PrepareFood(food);
+                }).Start();
             }
         }
     }
